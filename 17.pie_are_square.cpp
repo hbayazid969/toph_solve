0643c6b68d5ce26fe64f8ab4d15c06bcbf79226f
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-08-14 03:24:44 +0600

    Create 17.pie_are_square.cpp

---
+++ 17.pie_are_square.cpp
@@ -0,0 +1,13 @@
+//https://toph.co/p/pie-are-squared?statement=bn_bd
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	double r;
+	cin>>r;
+	long double pi=3.141592653589793,A;
+	A=pi*pow(r,2);
+	cout<<fixed<<setprecision(10)<<A<<endl;
+}
