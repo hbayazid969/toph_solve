5349de6975c82c3b983dcc29e5f0226c0a3f4ce1
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-08-13 21:47:34 +0600

    Create 3.divisors.cpp

---
+++ 3.divisors.cpp
@@ -0,0 +1,17 @@
+//https://toph.co/p/divisors
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i = 1; i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            cout<<i<<endl;
+        }
+    }
+}
