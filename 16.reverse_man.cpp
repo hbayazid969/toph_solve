2c7d041b4889eb9afb40d518695619eba50eb8f5
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-08-14 02:49:17 +0600

    Create 16.reverse_man.cpp

---
+++ 16.reverse_man.cpp
@@ -0,0 +1,11 @@
+//https://toph.co/p/reverse-man
+
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    string s;
+    getline(cin,s);
+    reverse(s.begin(),s.end());
+    cout<<s<<endl;
+}
