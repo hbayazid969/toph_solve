5116d4b329997335635f23d4916d92590d87e014
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-08-14 05:00:27 +0600

    Create 25.the_boat_filled.cpp

---
+++ 25.the_boat_filled.cpp
@@ -0,0 +1,11 @@
+//https://toph.co/p/the-boat-filled-with-love
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<"We Love You. We "<<n<<" Are Here For You."<<endl;
+}
