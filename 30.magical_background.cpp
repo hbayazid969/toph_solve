d28e6cc18453b6dc1a35c9a9415df01bff874ebf
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-08-15 01:10:32 +0600

    Create 30.magical_background.cpp

---
+++ 30.magical_background.cpp
@@ -0,0 +1,23 @@
+//https://toph.co/p/sofdor-alis-magical-blackboard
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    long long int a,b;
+
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+    long long int sum=0;
+    for(int i=0;i<b;i++){
+        sum+=a;
+    }
+    cout<<sum<<endl;
+    }
+
+
+}
